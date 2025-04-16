0cf5b16a7e68215f7f461ffc6a9b9077b1690e50
Māris Suss <marissuss@gmail.com>
2025-04-16 09:03:21 +0300

    contact update and translation

--- lang/lv/home.php
+++ lang/lv/home.php
@@ -3,7 +3,7 @@
 return [
     'projects' => 'Mani Projekti!',
     'i_am' => 'Es esmu',
-    'intro' => 'Māris Suss, full stack izstrādātājs ar fokusu uz backend. Es dzīvoju un elpoju ar PHP ar MySQL datu glabāšanai. Man ir pieredze ar dažādām tehnoloģijām un ietvariem, tostarp Laravel, Vue.js un Tailwind CSS.',
+    'intro' => 'Māris Suss, full stack izstrādātājs ar fokusu uz backend. Es dzīvoju un elpoju PHP ar MySQL datu glabāšanai. Man ir pieredze ar dažādām tehnoloģijām un ietvariem, tostarp Laravel, Vue.js un Tailwind CSS.',
     'clean_code' => 'Es koncentrējos uz tīru kodu un domāju par nākotni, lai mans kods darbotos lieliski un laika gaitā saglabātu savu vērtību. Citiem vārdiem sakot, es esmu lielisks komandas biedrs, jo stingri uzskatu, ka komandas darbs un savstarpēja cieņa ir jebkura lieliska projekta panākumu atslēga.',
     'hard_worker' => 'Cītīgs Darbinieks',
     'hard_worker_desc' => 'Es uzskatu, ka izaicinājumi ir lielākais skolotājs un virzītājspēks. JFK citāts, pēc kura dzīvoju - "Mēs izvēlamies doties uz Mēnesi šajā desmitgadē un darīt citas lietas nevis tāpēc, ka tās ir vieglas, bet tāpēc, ka tās ir grūtas."',
